3fab2c9795de1a769a0a9c247fa55468f1cf1515
Tijmen Verhoef <tijmenmenno@gmail.com>
2022-05-09 14:46:22 +0200

    experiment 1 is now finished for TCP

--- scripts/experiment_1.sh
+++ scripts/experiment_1.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-echo "tcp test"
-../src/tcp_throughput recv > /dev/null  &
-sleep 1
-../src/tcp_throughput send 127.0.0.1 > ../out/tcp_tp &
-echo "done"
+rm ../out/tcp_tp
+
+for i in {1..30}
+do
+    echo "Starting test" $i
+    ../src/tcp_throughput recv > /dev/null  &
+    sleep .5
+    ../src/tcp_throughput send 127.0.0.1 >> ../out/tcp_tp
+done
--- src/tcp_throughput.c
+++ src/tcp_throughput.c
@@ -7,14 +7,14 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define KB_SIZE(kb) (kb << 10)
-#define MB_SIZE(mb) (KB_SIZE(mb) << 10)
-#define GB_SIZE(gb) (MB_SIZE(gb) << 10)
+#define KiB_SIZE(kb) (kb << 10)
+#define MiB_SIZE(mb) (KiB_SIZE(mb) << 10)
+#define GiB_SIZE(gb) (MiB_SIZE(gb) << 10)
 
-// #define DEBUG_PRINTS
+//#define DEBUG_PRINTS
 #define PORT 8080
 #define START_TOKEN 0xAA
-#define DATA_ARRAY_SIZE KB_SIZE(2)
+#define DATA_ARRAY_SIZE GiB_SIZE(1)
 
 static uint8_t *data_array = NULL;
 
@@ -41,7 +41,10 @@ int validate_buffers(uint8_t* buffA, uint8_t* buffB, size_t buffALength, size_t 
     for (size_t i = 0; i < buffALength; i++)
     {
         if ((buffA[i] ^ buffB[i]) != 0x00)
+        {
+            printf("Invalid token @ %zu [0x%x != 0x%x]\n", i, buffA[i], buffB[i]);
             return 0;
+        }
     }
 
     return 1;
@@ -49,9 +52,7 @@ int validate_buffers(uint8_t* buffA, uint8_t* buffB, size_t buffALength, size_t 
 
 int data_send(char* remote_ip)
 {
-    size_t data_array_size = DATA_ARRAY_SIZE;
-
-    if (generate_data_array(data_array_size) < 0)
+    if (generate_data_array(DATA_ARRAY_SIZE) < 0)
         return -1;
 
     int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -70,69 +71,53 @@ int data_send(char* remote_ip)
     printf("connected!\n");
 #endif
 
-    size_t stride = KB_SIZE(1);
-    size_t array_idx = 0;
-
     struct timespec start_transfer;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start_transfer);
-    while (array_idx < data_array_size)
-    {
-        size_t send_buff_size = stride;
+    clock_gettime(CLOCK_REALTIME, &start_transfer);
+    ssize_t res = send(fd, data_array, DATA_ARRAY_SIZE, 0);
 
-        if (array_idx + stride > data_array_size)
-            send_buff_size = data_array_size - array_idx;
+    if (res < 0)
+        goto error;
 
-        int res = send(fd, (data_array + array_idx), send_buff_size, 0);
-        if (res < 0)
-            goto error;
+    size_t response_buffer_offset = 0;
+    uint8_t* response_buffer = calloc(DATA_ARRAY_SIZE, sizeof(uint8_t));
 
+    ssize_t recv_result = 0;
+    do {
+        recv_result = recv(fd, response_buffer + response_buffer_offset, DATA_ARRAY_SIZE - response_buffer_offset, 0);
+
+        response_buffer_offset += recv_result;
+        
 #ifdef DEBUG_PRINTS
-        printf("[Send][%zu bytes]\n", send_buff_size);
+        printf("[send] received %ld/%d bytes\n", response_buffer_offset, DATA_ARRAY_SIZE);
 #endif
+    } while(recv_result > 0 && response_buffer_offset != DATA_ARRAY_SIZE);
 
-        array_idx += send_buff_size;
+    if (recv_result < 0)
+    {
+        free(response_buffer);
+        goto error;
     }
 
-    size_t response_buffer_size = KB_SIZE(1);
-    size_t response_buffer_idx = 0;
-    uint8_t* response_buffer = calloc(response_buffer_size, sizeof(uint8_t));
-    int response_res = recv(fd, response_buffer, stride, 0);
-
-    while(response_res > 0 && response_buffer_size < DATA_ARRAY_SIZE)
-    {
-#ifdef DEBUG_PRINTS
-        printf(
-            "[Send][Echo response][%zu -> %zu][idx: %zu]\n", 
-            response_buffer_size, 
-            response_buffer_size + stride, 
-            response_buffer_idx
-        );
-#endif
-
-        uint8_t* temp = realloc(response_buffer, response_buffer_size + stride);
-
-        if (temp == NULL)
-        {
-            free(response_buffer);
-            goto error;
-        }
-
-        response_buffer_idx += stride;
-        response_buffer_size += stride;
-        response_buffer = temp;
-
-        response_res = recv(fd, (response_buffer + response_buffer_idx), stride, 0);
-    }
     struct timespec end_transfer;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end_transfer);
+    clock_gettime(CLOCK_REALTIME, &end_transfer);
 
-    if (validate_buffers(data_array, response_buffer, data_array_size, response_buffer_size) == 0)
+    if (validate_buffers(data_array, response_buffer, DATA_ARRAY_SIZE, DATA_ARRAY_SIZE) == 0)
     {
         printf("Buffers Invalid\n");
+        goto error;
     }
 
-    size_t seconds = end_transfer.tv_sec - start_transfer.tv_sec;
-    size_t ms = (end_transfer.tv_nsec - start_transfer.tv_nsec) / 1000000;
+    ssize_t seconds = end_transfer.tv_sec - start_transfer.tv_sec;
+    ssize_t ns = (end_transfer.tv_nsec - start_transfer.tv_nsec);
+
+    if (ns < 0)
+    {
+        ns += 1000000000;
+        seconds++;
+    }
+
+    ssize_t ms = ns / 1000000;
+
     printf("Buffers Valid @ %lus:%lums\n", seconds, ms);
 
 #ifdef DEBUG_PRINTS
@@ -154,6 +139,9 @@ error:
 int data_recv()
 {
     int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int enable_reuse = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &enable_reuse, sizeof(enable_reuse));
+
     int remote_fd = -1;
 
     struct sockaddr_in addr;
@@ -176,72 +164,55 @@ int data_recv()
         goto error;
 
 #ifdef DEBUG_PRINTS
-    printf("connected!\n");
+    printf("[recv] connected!\n");
 #endif
 
-    size_t stride = KB_SIZE(1);
-    size_t buffer_idx = 0;
-    size_t buffer_size = KB_SIZE(1);
-    uint8_t* recv_buffer = calloc(stride, sizeof(uint8_t));
-    int recv_result = recv(remote_fd, recv_buffer, stride, 0);
-    while (recv_result > 0 && buffer_size < DATA_ARRAY_SIZE)
-    {
-#ifdef DEBUG_PRINTS
-        printf("[Recv][%zu -> %zu][idx: %zu]\n", buffer_size, buffer_size + stride, buffer_idx);
-#endif
-        uint8_t *temp = realloc(recv_buffer, buffer_size + stride);
+    size_t recv_buff_offset = 0;
+    uint8_t* recv_buffer = calloc(DATA_ARRAY_SIZE, sizeof(uint8_t));
+
+    ssize_t recv_result = 0;
+    do {
+        recv_result = recv(remote_fd, recv_buffer + recv_buff_offset, DATA_ARRAY_SIZE - recv_buff_offset, 0);
+
+        recv_buff_offset += recv_result;
         
-        if (temp == NULL)
-        {
-            free(recv_buffer);
-            goto error;
-        }
-
-        buffer_idx += buffer_size;
-        buffer_size += stride;
-        recv_buffer = temp;
-
-        recv_result = recv(remote_fd, (recv_buffer + buffer_idx), stride, 0);
-    }
+#ifdef DEBUG_PRINTS
+        printf("[recv] received %ld/%d bytes\n", recv_buff_offset, DATA_ARRAY_SIZE);
+#endif
+    } while(recv_result > 0 && recv_buff_offset != DATA_ARRAY_SIZE);
 
 #ifdef DEBUG_PRINTS
-    printf("[Recv][Total buffer size: %zu bytes]\n", buffer_size);
+    printf("[recv] receiving done\n");
 #endif
 
-    buffer_idx = 0;
-    while (buffer_idx < buffer_size)
+    if (recv_result < 0)
     {
-        size_t send_buff_size = stride;
-
-        if (buffer_idx + stride > buffer_size)
-            send_buff_size = buffer_size - buffer_idx;
-
-        int res = send(remote_fd, (recv_buffer + buffer_idx), send_buff_size, 0);
-        if (res < 0)
-            goto error;
-
-#ifdef DEBUG_PRINTS
-        printf("[Recv][Echoing %zu bytes]\n", send_buff_size);
-#endif
-
-        buffer_idx += send_buff_size;
+        free(recv_buffer);
+        goto error;
     }
 
-#ifdef DEBUG_PRINTS
-    printf("Cleanup for recv\n");
-#endif
+    int send_result = send(remote_fd, recv_buffer, DATA_ARRAY_SIZE, 0);
+    if (send_result < 0)
+    {
+        free(recv_buffer);
+        goto error;
+    }
 
-    close(fd);
+    printf("Cleanup for recv\n");
+
     close(remote_fd);
+    close(fd);
     return 0;
 
 error:
-    if (fd >= 0)
-        close(fd);
-
     if (remote_fd >= 0)
         close(remote_fd);
+
+    if (fd >= 0)
+        close(fd);
     
+    perror(NULL);
+
     return -1;
 }
 
@@ -250,7 +221,7 @@ int main(int argc, char** argv)
     if (argc < 2)
         return -1;
 
-    printf("Starting TCP throughput test\n");
+    printf("---TCP THROUGHPUT---\n");
 
     if (strcmp(argv[1], "send") == 0)
         return data_send(argv[2]);
