f6aa77855a57c2b19706ec65e20a5b616dd968ba
Tijmen Verhoef <tijmenmenno@gmail.com>
2022-05-11 11:37:20 +0200

    worked on rgcp tp impl

--- src/rgcp_throughput.c
+++ src/rgcp_throughput.c
@@ -5,13 +5,63 @@
 #include <stdio.h>
 #include <rgcp/rgcp.h>
 
+int create_socket(char* ip)
+{
+    struct sockaddr_in mw_addr;
+    mw_addr.sin_addr.s_addr = inet_addr(ip);
+    mw_addr.sin_family = AF_INET;
+    mw_addr.sin_port = htons(8000);
+    return rgcp_socket(AF_INET, (struct sockaddr*)&mw_addr, sizeof(mw_addr));
+}
+
 int data_send(char* middleware_ip, char* middleware_group)
 {
+    int fd = create_socket(middleware_ip);
+
+    if (fd < 0)
+        goto error;
+
+    rgcp_group_info_t** group_infos = NULL;
+    ssize_t group_count = rgcp_discover_groups(fd, &group_infos);
+
+    if (group_count < 0)
+        goto error;
+
+    rgcp_group_info_t* target_group = NULL;
+    for (ssize_t i = 0; i < group_count; i++)
+    {
+        if (strcmp(middleware_group, group_infos[i]->m_pGroupName) == 0)
+        {
+            if (target_group && strcmp(target_group->m_pGroupName, group_infos[i]->m_pGroupName) == 0 && target_group->m_groupNameHash > group_infos[i]->m_groupNameHash)
+                continue;
+            else
+                target_group = group_infos[i];
+        }
+    }
+
+    if (rgcp_connect(fd, *target_group) < 0)
+        goto error;
+        
+    //
+
+    rgcp_disconnect(fd);
+    rgcp_close(fd);
+
     return 0;
+
+error:
+    perror(NULL);
+
+    rgcp_disconnect(fd);
+    rgcp_close(fd);
+
+    return -1;
 }
 
 int data_recv(char* middleware_ip, char* middleware_group)
 {
+    //int fd = create_socket(middleware_ip);
+
     return 0;
 }
 
