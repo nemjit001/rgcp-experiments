99bc1987928dae469b4a4e92efd37a6262596cdb
Tijmen Verhoef <tijmenmenno@gmail.com>
2022-05-24 20:22:36 +0200

    started setup stab test for das5

--- scripts/rgcp_stab_test.sh
+++ scripts/rgcp_stab_test.sh
@@ -2,16 +2,30 @@
 
 rm -r ../out/rgcp_stab_peers_*
 NumClients=30
+RunLocal=1
 
-for i in {1..3}
-do
-    echo "Starting RGCP Stability Test" $i
+if [ ! -z $1 ]
+then
+    NumClients=$(($1))
+fi
 
-    [ -d ../out/rgcp_stab_peers_$i ] || mkdir -p ../out/rgcp_stab_peers_$i
+if [ ! -z $2 ]
+then
+    RunLocal=$(($2))
+fi
+
+function DoLocalTest()
+{
+    if [ $# -ne 1 ]
+    then
+        exit 1
+    fi
+
+    local client_count=$1
     
-    for j in `seq 1 $NumClients`
+    for j in `seq 1 $client_count`
     do
-        echo "Starting Client" $j "out of" $NumClients
+        echo "Starting Client" $j "out of" $client_count
 
         if [ "$j" -lt "$NumClients" ]
         then
@@ -22,4 +36,35 @@ do
 
         sleep .1
     done
+}
+
+function DoDasTest()
+{
+    if [ $# -ne 1 ]
+    then
+        exit 1
+    fi
+
+    local client_count=$1
+
+    for j in `seq 1 $client_count`
+    do
+        echo "Starting Das5 Client" $j "out of" $client_count
+        srun ../src/rgcp_stability_client 60 1000 $j STAB_TEST_$i
+        sleep .1
+    done
+}
+
+for i in {1..3}
+do
+    echo "Starting RGCP Stability Test" $i "(" $NumClients $RunLocal ")"
+
+    [ -d ../out/rgcp_stab_peers_$i ] || mkdir -p ../out/rgcp_stab_peers_$i
+    
+    if [ $RunLocal -eq 1 ]
+    then
+        DoLocalTest $NumClients
+    else
+        DoDasTest $NumClients
+    fi
 done
