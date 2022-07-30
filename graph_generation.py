3eb70a0ddf3374c013c38cc2a07c127bf8372571
Tijmen Verhoef <tijmenmenno@gmail.com>
2022-07-30 17:11:44 +0200

    got basics for tp graphs done, working on stab exp graphs

--- graph_generation.py
+++ graph_generation.py
@@ -9,26 +9,85 @@ def get_col_avg(df: pd.DataFrame, column_name: str):
     count = len(df[column_name].to_list())
     return df[column_name].sum() / count
 
-def make_tp_graphs(datapoint_dir: str, output_dir: str):
-    val_to_string = lambda val : str(val)
+def make_tcp_tp_graph(datapoint_dir: str):
+    tp_df = pd.read_csv(datapoint_dir + 'tcp_tp.csv')
 
-    tcp_tp_df = pd.read_csv(datapoint_dir + 'tcp_tp.csv')
-    tcp_tp_df['test_num'] = tcp_tp_df["test_num"].map(val_to_string)
-    tcp_tp_fig = px.histogram(tcp_tp_df, x='test_num', y="time_ms", barmode="group")
-    tcp_avg_rtt = get_col_avg(tcp_tp_df, 'time_ms')
+    tp_df['test_num'] = tp_df["test_num"].map(lambda val : str(val))
+    avg_rtt = get_col_avg(tp_df, 'time_ms')
 
-    tcp_tp_fig.add_shape(
+    tp_fig = px.histogram(tp_df, x='test_num', y="time_ms", barmode="group")
+
+    tp_fig.add_shape(
         type='line',
         x0=0.0,
         x1=1.0,
-        y0=tcp_avg_rtt,
-        y1=tcp_avg_rtt,
+        y0=avg_rtt,
+        y1=avg_rtt,
         line= { "color": 'Red' },
         xref='paper',
         yref='y'
     )
 
-    tcp_tp_fig.show()
+    return tp_fig
 
-make_tp_graphs(DATAPOINT_DIR, OUTPUT_DIR)
+def make_rgcp_simple_tp_graph(datapoint_dir: str):
+    tp_df = pd.read_csv(datapoint_dir + 'rgcp_tp.csv')
+    tp_df = tp_df[tp_df['peer_count'] == 2]
 
+    if len(tp_df.index) == 0:
+        print("Warning: empty dataframe for simple rgcp tp graph")
+
+    tp_df['test_num'] = tp_df["test_num"].map(lambda val : str(val))
+    avg_rtt = get_col_avg(tp_df, 'time_ms')
+
+    tp_fig = px.histogram(tp_df, x='test_num', y="time_ms", barmode="group")
+
+    tp_fig.add_shape(
+        type='line',
+        x0=0.0,
+        x1=1.0,
+        y0=avg_rtt,
+        y1=avg_rtt,
+        line= { "color": 'Red' },
+        xref='paper',
+        yref='y'
+    )
+
+    return tp_fig
+
+def make_rgcp_avg_tp_graph(datapoint_dir: str):
+    rgcp_tp_df = pd.read_csv(datapoint_dir + 'rgcp_tp.csv')
+    
+    rgcp_tp_df_list = { count: rgcp_tp_df[rgcp_tp_df['peer_count'] == count] for count in rgcp_tp_df['peer_count'].unique() }
+    rgcp_avg_rtt = {
+        "peer_count": [],
+        "avg_time_ms": []
+    }
+
+    for key in rgcp_tp_df_list:
+        rgcp_avg_rtt['peer_count'].append(str(key))
+        rgcp_avg_rtt['avg_time_ms'].append(get_col_avg(rgcp_tp_df_list[key], 'time_ms'))
+
+    df = pd.DataFrame(rgcp_avg_rtt)
+    tp_fig = px.line(df, x='peer_count', y='avg_time_ms')
+
+    return tp_fig
+
+def make_tp_graphs(datapoint_dir: str) -> list:
+    return [
+        make_tcp_tp_graph(datapoint_dir),
+        make_rgcp_simple_tp_graph(datapoint_dir),
+        make_rgcp_avg_tp_graph(datapoint_dir)
+    ]
+
+def make_stab_graphs(datapoint_dir: str) -> list:
+    return []
+
+tp_graphs = make_tp_graphs(DATAPOINT_DIR)
+stab_graphs = make_stab_graphs(DATAPOINT_DIR)
+
+for graph in tp_graphs:
+    print(f"<graph @ {id(graph)}>")
+
+for graph in stab_graphs:
+    print(f"<graph @ {id(graph)}>")
