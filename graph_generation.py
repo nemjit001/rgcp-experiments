4fe4abec0886866c50ea06e4566102a86634fd50
Tijmen Verhoef <tijmenmenno@gmail.com>
2022-07-31 14:16:19 +0200

    working on CPU util plot, TODO: timestamp seconds from start on cpu util

--- graph_generation.py
+++ graph_generation.py
@@ -13,10 +13,10 @@ def get_col_avg(df: pd.DataFrame, column_name: str):
 def make_tcp_tp_graph(datapoint_dir: str):
     tp_df = pd.read_csv(datapoint_dir + 'tcp_tp.csv')
 
-    tp_df['test_num'] = tp_df["test_num"].map(lambda val : str(val))
     avg_rtt = get_col_avg(tp_df, 'time_ms')
 
     tp_fig = px.histogram(tp_df, x='test_num', y="time_ms", barmode="group")
+    tp_fig.update_xaxes(type='category')
 
     tp_fig.add_shape(
         type='line',
@@ -38,10 +38,10 @@ def make_rgcp_simple_tp_graph(datapoint_dir: str):
     if len(tp_df.index) == 0:
         print("Warning: empty dataframe for simple rgcp tp graph")
 
-    tp_df['test_num'] = tp_df["test_num"].map(lambda val : str(val))
     avg_rtt = get_col_avg(tp_df, 'time_ms')
 
     tp_fig = px.histogram(tp_df, x='test_num', y="time_ms", barmode="group")
+    tp_fig.update_xaxes(type='category')
 
     tp_fig.add_shape(
         type='line',
@@ -61,16 +61,21 @@ def make_rgcp_avg_tp_graph(datapoint_dir: str):
     
     rgcp_tp_df_list = { count: rgcp_tp_df[rgcp_tp_df['peer_count'] == count] for count in rgcp_tp_df['peer_count'].unique() }
     rgcp_avg_rtt = {
-        "peer_count": [],
-        "avg_time_ms": []
+        "peer_count": [ 0 ],
+        "avg_time_ms": [ 0 ]
     }
 
     for key in rgcp_tp_df_list:
-        rgcp_avg_rtt['peer_count'].append(str(key))
+        if key == 2:
+            continue
+
+        rgcp_avg_rtt['peer_count'].append(key)
         rgcp_avg_rtt['avg_time_ms'].append(get_col_avg(rgcp_tp_df_list[key], 'time_ms'))
 
     df = pd.DataFrame(rgcp_avg_rtt)
     tp_fig = px.line(df, x='peer_count', y='avg_time_ms')
+    tp_fig.update_xaxes(type='category', rangemode='tozero')
+    tp_fig.update_yaxes(rangemode='tozero')
 
     return tp_fig
 
@@ -81,8 +86,24 @@ def make_tp_graphs(datapoint_dir: str) -> dict:
         "rgcp_avg": make_rgcp_avg_tp_graph(datapoint_dir)
     }
 
+def make_cpu_avg_util_graph(datapoint_dir: str):
+    util_df = pd.read_csv(datapoint_dir + 'stab_cpu_load.csv')
+    util_df = util_df[util_df['cpu'] == "all"]
+
+    util_fig = px.line(
+        util_df,
+        x='timestamp',
+        y='usr',
+        color='peer_count'
+    )
+    
+    return util_fig
+
 def make_stab_graphs(datapoint_dir: str) -> dict:
-    return {}
+    return {
+        "cpu_avg_util_over_peers": make_cpu_avg_util_graph(datapoint_dir),
+        "mem_avg_util_over_peers": None
+    }
 
 if __name__ == '__main__':
     tp_graphs = make_tp_graphs(DATAPOINT_DIR)
